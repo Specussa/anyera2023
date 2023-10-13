54c07fc00c56cbc1ae57d920f6a1704175ed2a1a
Spec Ussa <serega.ussa@gmail.com>
2023-10-14 00:36:37 +0300

    fix

--- js/briefing.js
+++ js/briefing.js
@@ -126,7 +126,7 @@ if(!briefing){} else {
   }
 
   function updateValue() {
-    this.nextElementSibling.children[0].textContent = Math.max(1, Math.min(this.getAttribute('maxl'), this.value.length));
+    this.nextElementSibling.children[0].textContent = Math.max(0, Math.min(this.getAttribute('maxl'), this.value.length));
     if (this.value.length < this.getAttribute('minl')) {
       this.parentElement.classList.add('error');
       this.parentElement.classList.remove('success');
