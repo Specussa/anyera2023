c2958523ad41b6d4c07778d318a8af50ec05c8fa
Spec Ussa <serega.ussa@gmail.com>
2023-11-15 16:35:52 +0300

    build career

--- js/form.js
+++ js/form.js
@@ -1,8 +1,28 @@
+const finputs = document.querySelectorAll(".form__input");
+for (i = 0; i < finputs.length; i++) {
+  finputs[i].addEventListener("input", updateformValue);
+  finputs[i].nextElementSibling.children[1].textContent = finputs[i].getAttribute('maxl');
+}
+
+function updateformValue() {
+  this.nextElementSibling.children[0].textContent = Math.max(0, Math.min(this.getAttribute('maxl'), this.value.length));
+  if (this.value.length < this.getAttribute('minl')) {
+    this.parentElement.classList.add('error');
+    this.parentElement.classList.remove('success');
+    this.nextElementSibling.classList.remove('success');
+  } else {
+    this.parentElement.classList.remove('error');
+    this.parentElement.classList.add('success');
+    this.nextElementSibling.classList.add('success');
+  }
+}
+
+// start validate form header
 const form = document.getElementById('form');
 const username = document.getElementById('username');
 const phone = document.getElementById('phone');
 const text = document.getElementById('text');
-// start validate form header
+
 if(!form){} else {
   form.addEventListener('submit', e => {
   e.preventDefault();
@@ -41,23 +61,65 @@ if(!form){} else {
 // end validate form header
 
 // start validate form header
-  function updateformValue() {
-    this.nextElementSibling.children[0].textContent = Math.max(0, Math.min(this.getAttribute('maxl'), this.value.length));
-    if (this.value.length < this.getAttribute('minl')) {
-      this.parentElement.classList.add('error');
-      this.parentElement.classList.remove('success');
-      this.nextElementSibling.classList.remove('success');
+
+  var filesToUpload = [];
+  const fileInput = document.querySelector('.form__input_file');
+  const fileContainer = document.getElementById('form__file_container');
+
+  function domObserver(el, callback){
+      var done = function (){ callback(el); };
+      var observer = new MutationObserver(done);
+      observer.observe(el, { childList: true });
+  };
+  domObserver(fileContainer, function (el){
+    if (fileContainer.children[1]) {
+      fileContainer.children[0].remove();
+    }
+  });
+
+  function formatBytes(bytes, decimals = 2) {
+    if (bytes === 0) {
+      return '0';
     } else {
-      this.parentElement.classList.remove('error');
-      this.parentElement.classList.add('success');
-      this.nextElementSibling.classList.add('success');
+      var k = 1024;
+      var dm = decimals < 0 ? 0 : decimals;
+      var sizes = ['B', 'Kb', 'Mb', 'Gb', 'Tb'];
+      var i = Math.floor(Math.log(bytes) / Math.log(k));
+      return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
     }
   }
 
-  const finputs = document.querySelectorAll(".form__input");
+  fileInput.addEventListener('change', (e) => {
+    console.log(e.target.files.length);
+    for (let i = 0; i < e.target.files.length; i++) {
+      let myFile = e.target.files[i];
+      let myFileID = "FID" + (1000 + Math.random() * 9000).toFixed(0);
+      let myFileSize = formatBytes(e.target.files[i].size);
 
-  for (i = 0; i < finputs.length; i++) {
-    finputs[i].addEventListener("input", updateformValue);
-    finputs[i].nextElementSibling.children[1].textContent = finputs[i].getAttribute('maxl');
+      filesToUpload.push({
+        file: myFile,
+        FID: myFileID,
+        filesize: myFileSize
+      });
+    };
+    displayFiles();
+    // e.target.value = null;
+  });
+
+  const removeFile = (x) => {
+    for (let i = 0; i < filesToUpload.length; i++) {
+      if (filesToUpload[i].FID === x) {
+        filesToUpload.splice(i, 1);
+      }
+    }
+    displayFiles();
+  }
+
+  const displayFiles = () => {
+    console.log(fileContainer.innerHTML);
+    fileContainer.innerHTML = "";
+    for (let i = 0; i < filesToUpload.length; i++) {
+      fileContainer.innerHTML += `<li class="form__file_item"><span class="form__file_name">${filesToUpload[i].file.name}</span><span class="form__file_size">${filesToUpload[i].filesize}</span><button onclick="removeFile('${filesToUpload[i].FID}')"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7 17L12 12M12 12L17 7.00001M12 12L7 7M12 12L17 17" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/></svg></button></li>`;
+    }
   }
 // end validate form header
